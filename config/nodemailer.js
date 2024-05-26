020565df9a4f2429e4993b97c340c564fec358d9
DevikusumaSrimusuru <devikusumasrimusuru85@gmail.com>
2024-05-26 15:16:25 +0530

    first commit

---
+++ config/nodemailer.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SMTP_SERVER,
+  port: process.env.EMAIL_SMTP_PORT,
+  auth: {
+    user: process.env.EMAIL_SMTP_LOGIN,
+    pass: process.env.EMAIL_SMTP_PASSWORD
+  }
+});
+
+module.exports = transporter;
